6f088e8062c52edd58d3badf3e5669ac97bb7f8a
Will Marett <will.j.marett@gmail.com>
2023-06-27 22:23:59 -0500

    signupform

--- softwarepathfinders/src/App.js
+++ softwarepathfinders/src/App.js
@@ -1,10 +1,49 @@
-import React from 'react';
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <></>
+//   )
+// }
+
+// export default App;
+import React, { useState, useEffect } from 'react';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import SignUpForm from './components/auth/SignUpForm';
+import { authenticate } from './store/session';
 import './App.css';
+import {store} from './store/store';
 
 function App() {
-  return (
-    <></>
-  )
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async () => {
+      await dispatch(authenticate());
+      setLoaded(true);
+    })();
+  }, [dispatch]);
+
+  // if (!loaded) {
+  //   return null;
+  // }
+
+  return loaded && (
+    <Router>
+      <Provider store={store}>
+        <Routes>
+          <Route path='/sign-up' element={SignUpForm} exact={true}>
+            <SignUpForm />
+          </Route>
+        </Routes>
+      </Provider>
+    </Router>
+  );
 }
 
 export default App;
