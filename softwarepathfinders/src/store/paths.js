36334f288b02e90979125df0eac924a521814d95
Benjamin Lau <benjaminkhlau@gmail.com>
2023-06-27 21:38:52 -0500

    paths reducer added

---
+++ softwarepathfinders/src/store/paths.js
@@ -0,0 +1,143 @@
+// Action Types 
+const PATH_CREATE = "path/create"
+const PATH_READ = "path/read"
+const PATH_READ_ONE = "path/readOne"
+const PATH_UPDATE = "path/update"
+const PATH_DELETE = "path/delete"
+
+
+// Action Creators 
+const PathCreateACTION = (payload) => {
+    return {
+     type: PATH_CREATE,
+     payload
+    }
+}
+const PathReadACTION = (payload) => {
+    return {
+     type: PATH_READ,
+     payload
+    }
+}
+const PathReadOneACTION = (payload) => {
+    return {
+     type: PATH_READ_ONE,
+     payload
+    }
+}
+const PathUpdateACTION = (payload) => {
+    return {
+     type: PATH_UPDATE,
+     payload
+    }
+}
+const PathDeleteACTION = (payload) => {
+    return {
+     type: PATH_DELETE,
+     payload
+    }
+}
+
+// Thunk Action Creators 
+export const PathGetAllThunk = () => async dispatch => {
+    const response = await fetch(`/api/paths`, {
+        method: "GET"
+    })
+    const data = await response.json();
+    if (response.ok){
+        dispatch(PathGetAllThunk(data))
+    }
+
+    return data
+}
+
+export const PathGetOneThunk = (pathID) => async dispatch => {
+    const response = await fetch(`/api/paths/${pathID}`, {
+        method: "GET"
+    })
+    if (response.ok) {
+        const path = await response.json();
+        dispatch(PathReadOneACTION(path))
+        return path;
+    }
+}
+
+export const PathCreateThunk = (path) => async dispatch => {
+    const response = await fetch(`/api/paths`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(path)
+    })
+  
+    if (response.ok) {
+      const newPath = await response.json()
+      dispatch(PathCreateACTION(newPath))
+      return newPath
+    }
+    return response.json()
+}
+
+
+export const PathUpdateThunk = (payload, pathID) => async dispatch => {
+    const response = await fetch(`/api/paths/${pathID}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload)
+    });
+    if (response.ok) {
+        const path = await response.json();
+        dispatch(PathUpdateACTION(path))
+    }
+    return response
+}
+
+export const PathDeleteThunk = (pathID) => async dispatch => {
+    const response = await fetch(`/api/paths/${pathID}`, {
+        method: "DELETE"
+    });
+
+    if (response.ok) {
+        dispatch(PathDeleteACTION(pathID))
+    }
+}
+
+// Reducer 
+
+const initialState = {}
+
+const PathsReducer = (state = initialState, action) => {
+    let newState = {};
+    switch(action.type){
+        case PATH_READ: {
+            newState = {...action.payload}
+            return newState
+        }
+      	case PATH_CREATE: {
+        	newState = {...state}
+        	newState[action.payload.id] = action.payload
+            return newState;
+        }
+        case PATH_UPDATE: {
+        	newState = {...state}
+        	newState[action.payload.id] = action.payload
+            return newState;
+        }
+        case PATH_READ_ONE: {
+        	newState = {...state}
+        	newState[action.payload.id] = action.payload
+            return newState;
+        }
+        case PATH_DELETE: {
+            newState = { ...state }
+            delete newState[action.payload]
+            return newState;
+      	}
+
+    default:
+    return state;
+    }
+}
+
+export default PathsReducer;
--- softwarepathfinders/src/store/store.js
+++ softwarepathfinders/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import session from "./session"
+import PathsReducer from "./paths"
 
 
 export const store = configureStore({
   reducer: {
     session,
+    paths: PathsReducer
   },
 });
