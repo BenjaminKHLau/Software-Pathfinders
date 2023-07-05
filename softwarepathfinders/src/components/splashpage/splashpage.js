b2c8cd05e8fa6d64a84b4731eb91111577b39d0e
Benjamin Lau <benjaminkhlau@gmail.com>
2023-07-05 17:48:46 -0500

    testing optional chaining method to get information

--- softwarepathfinders/src/components/splashpage/splashpage.js
+++ softwarepathfinders/src/components/splashpage/splashpage.js
@@ -1,8 +1,15 @@
+import { useSelector } from "react-redux"
+
 function Splashpage(){
+
+    const session = useSelector(state => state.session)
+    const user = session.user ? session.user : null;
 
     return(
         <section>
         Welcome to Software Pathfinders!
+
+        <div>{user?.profile.Email}</div>
         </section>
     )
 }
