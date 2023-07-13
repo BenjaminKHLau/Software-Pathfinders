b06db020e9a2564c3df3d0abe9998b593b46e3ea
Benjamin Lau <benjaminkhlau@gmail.com>
2023-07-13 14:57:32 -0500

    hero image added with text

--- softwarepathfinders/src/components/splashpage/splashpage.js
+++ softwarepathfinders/src/components/splashpage/splashpage.js
@@ -1,22 +1,23 @@
-import { useSelector } from "react-redux"
-import colorful from "../../pics/colorfullaptop.jpg"
-import notes from "../../pics/noteslaptop.jpg"
-import study from "../../pics/studylaptop.jpg"
-import "./splashpage.css"
+import { useSelector } from "react-redux";
+import colorful from "../../pics/colorfullaptop.jpg";
+import notes from "../../pics/noteslaptop.jpg";
+import study from "../../pics/studylaptop.jpg";
+import "./splashpage.css";
 
-function Splashpage(){
+function Splashpage() {
+  const session = useSelector((state) => state.session);
+  const user = session.user ? session.user : null;
 
-    const session = useSelector(state => state.session)
-    const user = session.user ? session.user : null;
+  return (
+    <section>
+      {/* <img src={notes} alt="hero" id="hero"/> */}
+      <div id="hero">
+        <div className="hero-text">Welcome to Software Pathfinders!</div>
+      </div>
 
-    return(
-        <section>
-            <img src={notes} alt="hero" id="hero"/>
-        Welcome to Software Pathfinders!
-
-        <div>Hello, {user?.profile.FirstName}</div>
-        </section>
-    )
+      <div>Hello, {user?.profile.FirstName}</div>
+    </section>
+  );
 }
 
-export default Splashpage
+export default Splashpage;
